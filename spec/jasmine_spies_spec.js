a46e002aa44079d224f906aa92ee09abd43bcad5
lebogang-cloud <mphagol@gmail.com>
2020-01-28 11:14:36 +0200

    update commit

--- spec/jasmine_spies_spec.js
+++ spec/jasmine_spies_spec.js
@@ -7,22 +7,24 @@ describe("Dress to getBrand() and getSize Test", function() {
     spyOn(dress, "getBrand");
 
     dress.getBrand();
-    dress.getBrand()
-    dress.getBrand("Gucci", 8)
-
     expect(dress.getBrand).toHaveBeenCalled();
-    expect(dress.getBrand).toHaveBeenCalledTimes(3);
-    expect(dress.getBrand).toHaveBeenCalledWith("Gucci", 8);
   });
 
-  it("calls the getSize() function", function() {
-    spyOn(dress, "getSize");
+  it(" checks the number of times the getBrand() function is called", function() {
+    spyOn(dress, "getBrand");
 
-    dress.getSize("Gucci", 8)
+    dress.getBrand();
+    dress.getBrand();
+    dress.getBrand();
+    expect(dress.getBrand).toHaveBeenCalledTimes(3)
+  });
+
+  it(" checks the arguments getBrand() function has been called with", function() {
+    spyOn(dress, "getBrand");
+
     
-    expect(dress.getSize).toHaveBeenCalled();
-    expect(dress.getSize).toHaveBeenCalledTimes(1);
-    expect(dress.getSize).toHaveBeenCalledWith("Gucci", 8);
+    dress.getBrand("Gucci")
+    expect(dress.getBrand).toHaveBeenCalledWith("Gucci");
   });
     
 });
